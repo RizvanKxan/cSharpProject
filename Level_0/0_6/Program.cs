67d63cb29a976b33ba3a16846fb2b57e5f57e139
Leonid Gromyko <Dark-seizer@yandex.ru>
2020-09-29 18:48:04 +0300

    Added files.

---
+++ Level_0/0_6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*  Найдите сумму первых n натуральных чисел, которые делятся на 3 без остатка.
+ */
+namespace _0_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            int s;
+            int summ = 0;
+            int count = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Введите сколько натуральных чисел, делящихся на 3 без остатка, необходимо сложить: ");
+                if (!int.TryParse(Console.ReadLine(), out s) || ((s < 0) || (s % 1 != 0)))
+                {
+                    Console.WriteLine("Условия не соблюдены. Повторите ввод.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    if ((s > 0) && (s % 1 == 0)) break;
+                }
+            }
+            Console.Write("Натуральные числа, которые складывались: ");
+            int i = 0;
+            while (true)
+            {
+                if (i % 3 == 0)
+                {
+                    ++count;
+                    if (count < s)
+                    {
+                        Console.Write($"{i}, ");
+                        summ += i;
+                    }
+                    else if (count == s)
+                    {
+                        Console.Write($"{i}. ");
+                        summ += i;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+                ++i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Сумма чисел равна: {summ}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Level_0/0_7/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*  Дано натуральное число n. Получить все его натуральные делители.
+ */
+namespace _0_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            int s;
+            List<int> deliteli = new List<int>(5);
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Введите натуральное число для которого надо найти делители: ");
+                if (!int.TryParse(Console.ReadLine(), out s) || ((s < 0) || (s % 1 != 0)))
+                {
+                    Console.WriteLine("Условия не соблюдены. Повторите ввод.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    if ((s > 0) && (s % 1 == 0)) break;
+                }
+            }
+            for (int i = 1; i <= Math.Sqrt(s); i++)
+            {
+                if (s % i == 0)
+                {
+                    deliteli.Add(i);
+                    //для корня из s не существует парного делителя
+                    if (i * i != s)
+                    {
+                        deliteli.Add(s / i);
+                    }
+                }
+            }
+
+            deliteli.Sort();
+            Console.WriteLine($"Количество делителей: {deliteli.Count}");
+            Console.Write($"Делители числа {s}: ");
+            for (int i = 0; i < deliteli.Count; ++i)
+            {
+                if (i < (deliteli.Count - 1))
+                {
+                    Console.Write($" {deliteli[i]},");
+                }
+                else
+                {
+                    Console.Write($" {deliteli[i]}.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
